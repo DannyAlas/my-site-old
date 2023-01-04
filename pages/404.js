86f0c1024b8b11a5033cf57d3ad1b12a287a4621
Jazzy <81212794+DannyAlas@users.noreply.github.com>
2023-01-03 19:13:27 -0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -11,8 +11,8 @@ import {
 const NotFound = () => {
   return (
     <Container>
-      <Heading as="h1">Not found...</Heading>
-      <Text>The page you&apos;re looking for was not found, soory.</Text>
+      <Heading as="h1">beep-boop Page Fault...</Heading>
+      <Text>The page you&apos;re looking for was not found, sorry.</Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <NextLink href="/" passHref>
