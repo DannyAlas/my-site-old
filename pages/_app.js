7a4aa95d5f737a12b8c497f12408324903832fac
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2023-04-21 15:05:39 -0600

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
+import { Analytics } from '@vercel/analytics/react';
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
@@ -11,6 +12,7 @@ function Website({ Component, pageProps, router }) {
   return (
     <Chakra cookies={pageProps.cookies}>
       <Fonts />
+      <Analytics mode={'production'} debug={true} />
       <Layout router={router}>
         <AnimatePresence
           exitBeforeEnter
